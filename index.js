d61a8a536cf82e6b311155bab0dd34483ed43945
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-11-26 18:09:14 +0600

    Added API's for user role and status changes.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,100 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    // LOADING COLLECTIONS
+    const userCollection = client
+      .db("BloodBridgeDB")
+      .collection("userCollection");
+
+    // ----------------------- USER RELATED APIS -----------------------------
+    // ++USER POST API++
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // +USER PATCH BLOCK API++
+    app.patch("/users/block/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const updatedUser = {
+        $set: {
+          status: "blocked",
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedUser);
+      res.send(result);
+    });
+
+    // +USER PATCH UNBCLOCK API++
+    app.patch("/users/unblock/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const updatedUser = {
+        $set: {
+          status: "active",
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedUser);
+      res.send(result);
+    });
+
+    // +USER PATCH MAKE VOLUNTEER API++
+    app.patch("/users/volunteer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const updatedUser = {
+        $set: {
+          role: "volunteer",
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedUser);
+      res.send(result);
+    });
+
+    // +USER PATCH MAKE DONOR API++
+    app.patch("/users/donor/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const updatedUser = {
+        $set: {
+          role: "donor",
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedUser);
+      res.send(result);
+    });
+
+    // +USER PATCH ADMIN API++
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const updatedUser = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(query, updatedUser);
+      res.send(result);
+    });
+
+    // ++USER GET API++
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
