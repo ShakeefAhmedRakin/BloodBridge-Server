1e8c3ff2a843446ce9ec1a2da24ad7258fe51047
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-11-30 12:06:03 +0600

    Completed all donor related api calls and secured them.

--- index.js
+++ index.js
@@ -55,14 +55,35 @@ async function run() {
       });
     };
 
-    // after token, then verify admin
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === "admin";
       if (!isAdmin) {
-        return res.status(403).send({ message: "forbidden acesss" });
+        return res.status(403).send({ message: "forbidden acess" });
+      }
+      next();
+    };
+
+    const verifyDonor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isDonor = user?.role === "donor";
+      if (!isDonor) {
+        return res.status(403).send({ message: "forbidden acess" });
+      }
+      next();
+    };
+
+    const verifyDonorOrAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isDonorOrAdmin = user.role === "donor" || user.role === "admin";
+      if (!isDonorOrAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
     };
@@ -232,6 +253,26 @@ async function run() {
       }
     );
 
+    // ++USER GET DONOR API++
+    app.get(
+      "/users/donor/:email",
+      verifyToken,
+      verifyDonor,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "unauthorized" });
+        }
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let isDonor = false;
+        if (user) {
+          isDonor = user?.role === "donor";
+        }
+        res.send({ isDonor });
+      }
+    );
+
     // ----------------------- DONATION RELATED APIS -----------------------------
     // ++REQUEST DONATION POST API++
     app.post("/donation-requests", async (req, res) => {
@@ -239,6 +280,210 @@ async function run() {
       const result = await requestCollection.insertOne(requestInfo);
       res.send(result);
     });
+
+    // ++REQUEST DONATION GET ALL API++
+    app.get("/donation-requests", async (req, res) => {
+      const result = await requestCollection.find().toArray();
+      res.send(result);
+    });
+
+    // ++REQUEST DONATION GET SINGLE API++
+    app.get("/donation-requests/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+          res.status(400).send({ error: "Invalid product ID" });
+          return;
+        }
+
+        const query = { _id: new ObjectId(id) };
+        const result = await requestCollection.findOne(query);
+
+        if (!result) {
+          res.status(404).send({ error: "Product not found" });
+          return;
+        }
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Internal server error" });
+      }
+    });
+
+    // ++REQUEST DONATION STATUS TO INPROGRESS API++
+    app.put(
+      "/donation-requests/inprogress/:id",
+      verifyToken,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = {
+          _id: new ObjectId(id),
+        };
+        const options = { upsert: true };
+        const body = req.body;
+        const updatedDonation = {
+          $set: {
+            request_status: "inprogress",
+            donor_name: body.donor_name,
+            donor_email: body.donor_email,
+          },
+        };
+        const result = await requestCollection.updateOne(
+          query,
+          updatedDonation,
+          options
+        );
+
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION GET ALL BASED ON EMAIL++
+    app.get(
+      "/user/donation-requests",
+      verifyToken,
+      verifyDonor,
+      async (req, res) => {
+        query = { requester_email: req.query.email };
+
+        if (req.query?.filter) {
+          query.request_status = req.query.filter;
+        }
+
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+
+        const result = await requestCollection
+          .find(query)
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION GET FIRST THREE LATEST DONATIONS BASED ON EMAIL++
+    app.get(
+      "/user/donation-requests/recent",
+      verifyToken,
+      verifyDonor,
+      async (req, res) => {
+        query = { requester_email: req.query.email };
+
+        const result = await requestCollection
+          .find(query)
+          .sort({ creation_time: -1 }) // Sort by creation_time in descending order
+          .limit(3) // Limit the results to three entries
+          .toArray();
+
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION GET ALL BASED ON EMAIL++ (COUNT)
+    app.get(
+      "/user/donation-requests/count",
+      verifyToken,
+      verifyDonor,
+      async (req, res) => {
+        let query = {};
+        if (req.query?.email) {
+          query.requester_email = req.query.email;
+        }
+
+        if (req.query?.filter) {
+          console.log(req.query.filter);
+          query.request_status = req.query.filter;
+        }
+
+        const count = await requestCollection.countDocuments(query);
+        res.send({ count });
+      }
+    );
+
+    // ++REQUEST DONATION PATCH STATUS TO DONE API++
+    app.patch(
+      "/donation-requests/patch-done/:id",
+      verifyToken,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = {
+          _id: new ObjectId(id),
+        };
+        const updatedRequest = {
+          $set: {
+            request_status: "done",
+          },
+        };
+        const result = await requestCollection.updateOne(query, updatedRequest);
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION PATCH STATUS TO CANCELLED API++
+    app.patch(
+      "/donation-requests/patch-cancel/:id",
+      verifyToken,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = {
+          _id: new ObjectId(id),
+        };
+        const updatedRequest = {
+          $set: {
+            request_status: "cancelled",
+          },
+        };
+        const result = await requestCollection.updateOne(query, updatedRequest);
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION DELETE API++
+    app.delete(
+      "/donation-requests/delete/:id",
+      verifyToken,
+      verifyDonorOrAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await requestCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+
+    // ++REQUEST DONATION UPDATE API++
+    app.put(
+      "/donation-requests/update/:id",
+      verifyToken,
+      verifyDonorOrAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedRequest = req.body;
+        const details = {
+          $set: {
+            requester_message: updatedRequest.requester_message,
+            recipient_name: updatedRequest.recipient_name,
+            hospital_name: updatedRequest.hospital_name,
+            address: updatedRequest.address,
+            recipient_district: updatedRequest.recipient_district,
+            recipient_upazilla: updatedRequest.recipient_upazilla,
+            request_date: updatedRequest.request_date,
+            request_time: updatedRequest.request_time,
+          },
+        };
+
+        const result = await requestCollection.updateOne(
+          query,
+          details,
+          options
+        );
+        res.send(result);
+      }
+    );
 
     await client.db("admin").command({ ping: 1 });
     console.log(
